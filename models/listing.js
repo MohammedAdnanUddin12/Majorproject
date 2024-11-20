5c40593196b28acbe5fc33a3a41d0eb2a9bba02a
MohammedAdnanUddin12 <mohammedadnanuddin12@gmail.com>
2024-11-20 15:49:36 +0530

    Major Project delta

---
+++ models/listing.js
@@ -0,0 +1,20 @@
+const mongoose = require ("mongoose");
+
+
+
+const listingSchema = new mongoose.Schema({
+    title: { type: String, required: true },
+    description: { type: String, required: true },
+    image: {
+        url: { type: String, required: true },
+    },
+    price: { type: Number, required: true },
+    
+    location: { type: String, required: true },
+    country: { type: String, required: true },
+});
+
+  
+const Listing = mongoose.model("Listing", listingSchema);
+module.exports = Listing;
+//model or class
